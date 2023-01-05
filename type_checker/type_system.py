7e6a86913309b2878745d32dccf22febb97ed975
Joseph E <1jojorun@gmail.com>
2023-01-05 03:43:08 -0800

    Type-check system designed

---
+++ type_checker/type_system.py
@@ -0,0 +1,49 @@
+class TypeSystem:
+    """
+    A system that manages the valid types in the language and validates
+    typecasting
+    """
+    def __init__(self) -> None:
+        self.declared_types = {
+            'void': [],
+            'int': ['char', 'float'],
+            'float': ['int'],
+            'char': ['int'],
+        }
+
+    def add_type(self, new_type):
+        self.declared_types[new_type] == []
+
+    def get_base_type_from_ptr(self, type):
+        """
+        Returns a string without any '*' in it
+        """
+        base_type = ''
+        for c in type:
+            if c == '*':
+                break
+            base_type += c
+        return base_type
+
+    def is_valid_type(self, type):
+        return self.get_base_type_from_ptr(type) in self.declared_types
+
+    def is_pointer_type(self, type):
+        return '*' in type
+
+    def type_matches(self, type_a, type_b):
+        return type_a == type_b
+
+    def type_castable(self, type, desired_type):
+        if not self.is_valid_type(desired_type):
+            return False
+
+        cast_list = self.declared_types[type]
+        if self.type_matches(type, desired_type):
+            return True
+        elif self.is_pointer_type(type) and desired_type in ('int', 'void*'):
+            return True
+        elif type == 'void*' and self.is_pointer_type(desired_type):
+            return True
+        else:
+            return desired_type in cast_list
