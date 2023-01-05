dcd5972164f56974af5d61cce25ee8f1ed301bb2
Joseph E <1jojorun@gmail.com>
2023-01-04 18:58:33 -0800

    Writing Ast and Parser

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Imports
 from lexer.lexer import Lexer
-# from parser.parser
+from parser.parser import Parser
 
 # Main Code
 lexer = Lexer("Console", "1++>=&&||2.8*4%()")
 tokens = lexer.get_tokens()
 print(tokens)
+
+parser = Parser(tokens)
--- parser/parser.py
+++ parser/parser.py
@@ -1,7 +1,14 @@
 class Parser:
     def __init__(self, tokens: list):
         self.tokens = tokens
-        self.index = 0
+        self.index = -1
+        self.cur = None
+        self.get_next()
 
     def hasNext():
         return
+
+    def getNext():
+
+    def look_ahead():
+        
